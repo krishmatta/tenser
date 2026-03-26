be161874b197749425a4c65ff0961ff48b56196e
Krish Matta <self@krishmatta.net>
2026-03-26 18:37:18 -0400

    Add default stride

--- tensor/src/shape.rs
+++ tensor/src/shape.rs
@@ -4,13 +4,19 @@ pub trait Shape {
     const RANK: usize;
 
     fn size(&self) -> usize;
+
+    fn default_stride(&self) -> Vec<usize>;
 }
 
 impl Shape for () {
     const RANK: usize = 0;
 
     fn size(&self) -> usize {
-        0
+        1
+    }
+
+    fn default_stride(&self) -> Vec<usize> {
+        vec![]
     }
 }
 
@@ -20,6 +26,10 @@ impl<D1: Dimension> Shape for (D1,) {
     fn size(&self) -> usize {
         self.0.get_value()
     }
+
+    fn default_stride(&self) -> Vec<usize> {
+        vec![1]
+    }
 }
 
 impl<D1: Dimension, D2: Dimension> Shape for (D1, D2,) {
@@ -27,6 +37,11 @@ impl<D1: Dimension, D2: Dimension> Shape for (D1, D2,) {
 
     fn size(&self) -> usize {
         self.0.get_value() * self.1.get_value()
+    }
+
+    fn default_stride(&self) -> Vec<usize> {
+        let d2 = self.1.get_value();
+        vec![d2, 1]
     }
 }
 
@@ -36,6 +51,12 @@ impl<D1: Dimension, D2: Dimension, D3: Dimension> Shape for (D1, D2, D3) {
     fn size(&self) -> usize {
         self.0.get_value() * self.1.get_value() * self.2.get_value()
     }
+
+    fn default_stride(&self) -> Vec<usize> {
+        let d2 = self.1.get_value();
+        let d3 = self.2.get_value();
+        vec![d2 * d3, d3, 1]
+    }
 }
 
 impl<D1: Dimension, D2: Dimension, D3: Dimension, D4: Dimension> Shape for (D1, D2, D3, D4) {
@@ -43,5 +64,12 @@ impl<D1: Dimension, D2: Dimension, D3: Dimension, D4: Dimension> Shape for (D1, 
 
     fn size(&self) -> usize {
         self.0.get_value() * self.1.get_value() * self.2.get_value() * self.3.get_value()
+    }
+
+    fn default_stride(&self) -> Vec<usize> {
+        let d2 = self.1.get_value();
+        let d3 = self.2.get_value();
+        let d4 = self.3.get_value();
+        vec![d2 * d3 * d4, d3 * d4, d4, 1]
     }
 }
